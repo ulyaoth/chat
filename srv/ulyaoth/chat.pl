12917ad44967c6cce1b5ef04f182b5f8d657ec8b
Sjir Bagmeijer <sbagmeijer@ulyaoth.co.kr>
2016-01-04 15:42:37 +0100

    Mojolicious.

--- srv/ulyaoth/chat.pl
+++ srv/ulyaoth/chat.pl
@@ -1,32 +1,41 @@
 #!/usr/bin/env perl
+
+# Specify the additional Perl modules in order to run the chat.
 use Mojolicious::Lite;
+use YAML::Tiny;
+
+# Open the configuration file and put everything in $yaml.
+my $configuration = YAML::Tiny->read( '/etc/ulyaoth/config.yaml' );
+
+# All required variables from the config file.
+my $mytitle = $configuration->[0]->{variables}->{title};
 
 get '/' => sub {
   my $c = shift;
-  $c->render(template => 'login', css => 'login.css');
+  $c->render(template => 'login', css => 'login.css', title => $mytitle);
 };
 
 get '/login' => sub {
   my $c = shift;
-  $c->render(template => 'login');
+  $c->render(template => 'login', css => 'login.css', title => $mytitle);
 };
 
 get '/register' => sub {
   my $c = shift;
-  $c->render(template => 'login');
+  $c->render(template => 'login', css => 'login.css', title => $mytitle);
 };
 
 get '/lostpassword' => sub {
   my $c = shift;
-  $c->render(template => 'login');
+  $c->render(template => 'login', css => 'login.css', title => $mytitle);
 };
 
 app->start;
 __DATA__
 
+# Template section below here.
 @@ login.html.ep
 % layout 'default';
-% title '$title';
 <h3>Password currently not required for non registered users.</h3>
 <div class="container">
 	<section id="content">
@@ -54,5 +63,7 @@ __DATA__
     <title><%= title %></title>
     <link rel="stylesheet" type="text/css" href="css/<%= $css %>" />
   </head>
-  <body><%= content %></body>
+  <body>
+    <%= content %>
+  </body>
 </html>
